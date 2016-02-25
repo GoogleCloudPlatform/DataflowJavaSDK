a0ee9e43bc76f1970b5f1eabaed9c4f496462867
Raghu Angadi <rangadi@google.com>
2016-02-25 12:37:57 -0800

    KafkaRecord

---
+++ contrib/kafka/src/main/java/com/google/cloud/dataflow/contrib/kafka/KafkaRecord.java
@@ -0,0 +1,50 @@
+package com.google.cloud.dataflow.contrib.kafka;
+
+import java.io.Serializable;
+
+/**
+ * TODO(rangadi)
+ * @author rangadi
+ */
+public class KafkaRecord<K, V> implements Serializable {
+
+  private final String topic;
+  private final int partition;
+  private final long offset;
+  private final K key;
+  private final V value;
+
+  public KafkaRecord(
+      String topic,
+      int partition,
+      long offset,
+      K key,
+      V value) {
+
+    this.topic = topic;
+    this.partition = partition;
+    this.offset = offset;
+    this.key = key;
+    this.value = value;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public int getPartition() {
+    return partition;
+  }
+
+  public long getOffset() {
+    return offset;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+}
