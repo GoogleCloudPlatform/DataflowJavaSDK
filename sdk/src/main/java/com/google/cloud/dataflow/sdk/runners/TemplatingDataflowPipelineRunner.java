dbd078a7fefc6d8139ebe8bd5729a144919a60d3
sammcveety <sam.mcveety@gmail.com>
2016-08-19 18:37:14 -0400

    Do not return null, return a useless Job instead.

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/TemplatingDataflowPipelineRunner.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/TemplatingDataflowPipelineRunner.java
@@ -16,18 +16,22 @@ package com.google.cloud.dataflow.sdk.runners;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import com.google.api.services.dataflow.Dataflow;
 import com.google.auto.service.AutoService;
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineDebugOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsValidator;
 import com.google.cloud.dataflow.sdk.transforms.PTransform;
+import com.google.cloud.dataflow.sdk.util.MonitoringUtil;
 import com.google.cloud.dataflow.sdk.values.PInput;
 import com.google.cloud.dataflow.sdk.values.POutput;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link PipelineRunner} that's like {@link DataflowPipelineRunner} but only stores a template of
@@ -71,12 +75,59 @@ public class TemplatingDataflowPipelineRunner extends PipelineRunner<DataflowPip
     return new TemplatingDataflowPipelineRunner(dataflowPipelineRunner, options);
   }
 
+  private static class TemplateJob extends DataflowPipelineJob {
+    static String ERROR =
+      "The result of template creation should not be used.";
+    
+    TemplateJob() {
+      super(null, null, null, null);
+    }
+
+    @Override
+    public String getJobId() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public String getProjectId() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public DataflowPipelineJob getReplacedByJob() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public Dataflow getDataflowClient() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public State waitToFinish(
+      long timeToWait,
+      TimeUnit timeUnit,
+      MonitoringUtil.JobMessagesHandler messageHandler) {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public void cancel() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+
+    @Override
+    public State getState() {
+      throw new UnsupportedOperationException(ERROR);
+    }
+  }
+
   @Override
   public DataflowPipelineJob run(Pipeline p) {
     dataflowPipelineRunner.setHooks(new TemplateHooks());
     final DataflowPipelineJob job = dataflowPipelineRunner.run(p);
     LOG.info("Template successfully created.");
-    return null;
+    return new TemplateJob();
   }
 
   @Override
